c6ea21e339abb628da5512a7106ab91cd2fd3557
Ulrich Bittner <ulrich.bittner@me.com>
2017-12-03 13:51:19 +0100

    update

--- APCUPSD/module.php
+++ APCUPSD/module.php
@@ -73,7 +73,7 @@ class APCUPSD extends IPSModule
 	public function CheckStatus()
 	{
 		$result = $this->GetStatus();
-		return $result;
+
 		// check empty array
 		SetValue($this->GetIDForIdent("UPSName"), $result["UPSNAME"]);
 		SetValue($this->GetIDForIdent("UPSModel"), $result["MODEL"]);
@@ -81,7 +81,7 @@ class APCUPSD extends IPSModule
 		// missing boolean alert
 		SetValue($this->GetIDForIdent("UPSTimeLeft"), $result["TIMELEFT"]);
 
-
+		return $result;
 
 		/*
 		$status = $dataArray["STATUS"] ;
